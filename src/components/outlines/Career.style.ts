66222651ec13c56f56dbe273d17848337275d25d
Dochanii <dmlwhd010@naver.com>
2026-02-03 16:43:56 +0900

    feat: career 컴포넌트 디자인 완료

--- src/components/outlines/Career.style.ts
+++ src/components/outlines/Career.style.ts
@@ -3,5 +3,55 @@ import { css } from '@emotion/react';
 export const careerContainer = css`
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     background-color: #000;
+    
+`;
+export const careerTitle = css`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 8rem;
+    h2{
+        font-size: 4rem;
+        color: #fff;
+    }
+`;
+
+export const careerItemContainer = css`
+    width: 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    // background-color: red;
+`;
+
+export const careerProfile = css`
+    width: 45%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+export const careerList = css`
+    width: 55%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border-left: 2px solid gray;
+    padding : 0 2rem;
+`;
+export const careerListItem = css`
+    list-style: none;
+    line-height: 2;
+    font-size:2rem;
+    color: #fff;
+    padding : 0 2rem;
+
 `;
--- src/components/outlines/Career.tsx
+++ src/components/outlines/Career.tsx
@@ -1,7 +1,31 @@
+
+import ProfileCircle from "../ProfileCircle/ProfileCircle";
+import { careerContainer, careerItemContainer, careerTitle, careerProfile, careerList, careerListItem } from "./Career.style";
+import profileImage from "../../assets/images/profile.png";
+
 const Career = () => {
     return (
-        <div>
-            <h1>Career</h1>
+        <div css={careerContainer}>
+            <div css={careerTitle}>
+                <h2>CAREER</h2>
+            </div>
+            <div css={careerItemContainer}>
+                <div css={careerProfile}>
+                    <ProfileCircle image={profileImage} />
+
+                </div>
+                <ul css={careerList}>
+                    <li css={careerListItem}>
+                        <span>🎓 Department of Software in Sejong Univ.</span>
+                    </li>
+                    <li css={careerListItem}>
+                        <span>👨🏻‍💻 Sejong Univ. LikeLion 12nd</span>
+                    </li>
+                    <li css={careerListItem}>
+                        <span>🏆 2025 해커톤 챌린져스 해키토키 - 특별상 수상</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
