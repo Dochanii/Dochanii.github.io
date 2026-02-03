dc579362f52b96b8256e00891aa859c83ec6058f
Dochanii <dmlwhd010@naver.com>
2026-02-03 16:45:42 +0900

    feat: 프로필 이미지 들어갈 공간 컴포넌트 제작

---
+++ src/components/ProfileCircle/ProfileCircle.style.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export const profileCircleContainer = css`
+    width: 25rem;
+    height: 25rem;
+    background-color: #fff;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5rem;
+    padding: 2rem;
+`;
+
+export const profileImage = css`
+    width: 90%;
+    height: 90%;
+    object-fit: cover;
+`;
---
+++ src/components/ProfileCircle/ProfileCircle.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { profileCircleContainer, profileImage } from './ProfileCircle.style'
+
+const ProfileCircle = (props: { image: string }) => {
+  return (
+    <div css={profileCircleContainer}>
+        <img css={profileImage} src={props.image} alt="profile" />
+    </div>
+  )
+}
+
+export default ProfileCircle
