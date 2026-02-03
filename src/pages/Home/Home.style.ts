ee427c366956f89f4645c9533fa63e38b54a3675
Dochanii <dmlwhd010@naver.com>
2026-02-03 16:46:46 +0900

    feat: Home 페이지에 Swiper 적용 및 세팅

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -3,11 +3,43 @@ import {css} from '@emotion/react';
 export const homeContainer = css`
     position: relative;
     width: 100%;
-    height: 100%;
+    min-height: 100dvh;
     background-color: #000;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     overflow: hidden;
+`;
+
+export const swiperContainer = css`
+    width: 100%;
+    height: 100vh;
+    
+    .swiper-slide {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        background-color: #000;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+    }
+    
+    .swiper-pagination {
+        right: 20px !important;
+        left: auto !important;
+        width: auto !important;
+    }
+    
+    .swiper-pagination-bullet {
+        background: #fff;
+        opacity: 0.5;
+    }
+    
+    .swiper-pagination-bullet-active {
+        opacity: 1;
+    }
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,52 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, Pagination } from "swiper/modules";
+import NavBar from "../../components/navBar/NavBar";
 import About from "../../components/outlines/About/About";
+import Career from "../../components/outlines/Career";
 import Snow from "../../components/Snow/Snow";
-import { homeContainer } from "./Home.style"
+import { swiperContainer } from "./Home.style";
+// @ts-ignore
+import "swiper/css";
+// @ts-ignore
+import "swiper/css/pagination";
+import Skill from "../../components/outlines/Skills/Skill";
 
 const Home = () => {
+    const [swiper, setSwiper] = useState<any>(null);
+
     return (
-        <div css={homeContainer}>
-            <Snow />
-            <About />
-        </div>
-    )
-}
+        <>
+            <NavBar />
+            <Swiper
+                onSwiper={(swiper) => setSwiper(swiper)}
+                direction="vertical"
+                slidesPerView={1}
+                mousewheel={true}
+                pagination={{
+                    clickable: true,
+                }}
+                speed={1000}
+                modules={[Mousewheel, Pagination]}
+                css={swiperContainer}
+            >
+                <SwiperSlide>
+                    <Snow />
+                    <About />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Snow />
+                    <Career />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Snow />
+                    <Skill />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
 
 export default Home;
