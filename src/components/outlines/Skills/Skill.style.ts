45a2c8717cc36a51df69808ee81e76aa7fc8aae8
Dochanii <dmlwhd010@naver.com>
2026-02-03 16:46:14 +0900

    feat: Skill 컴포넌트 초기 제작

---
+++ src/components/outlines/Skills/Skill.style.ts
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+export const skillContainer = css`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+export const skillTitle = css`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: red;
+`;
---
+++ src/components/outlines/Skills/Skill.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { skillContainer, skillTitle } from './Skill.style';
+
+const Skill = () => {
+    return (
+        <div css={skillContainer}>
+            <div css={skillTitle}>
+                <h2>SKILLS</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Skill;
